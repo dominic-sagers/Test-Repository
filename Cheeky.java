7de0f238b0ddf85078128749f66bc6b2616d2ba9
dcsagers <d.sagers@student.maastrichtuniversity.nl>
2022-04-30 20:08:01 +0200

    Added some comments to Cheeky.java

--- Cheeky.java
+++ Cheeky.java
@@ -1,5 +1,6 @@
 public class Cheeky{
     public static void main(String[] args) {
+        //This code is perfect because sonya is perfect
         String secret = "Sonya is the most beautiful girl in all of DKE and she's mine";
         int amountOfTimesICanSayTheSecretBeforeIPassOut = 3087;
         for(int i = 0; i<amountOfTimesICanSayTheSecretBeforeIPassOut;i++){
